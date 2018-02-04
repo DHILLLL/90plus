15e5762d33aeceaac93dae06f3145dce521ad347
DHILLLL <635901193@qq.com>
2018-02-04 20:44:18 +0800

    2018年2月4日20:44:03 登录

--- app/src/main/java/com/whuLoveStudyGroup/app/LoginActivity.java
+++ app/src/main/java/com/whuLoveStudyGroup/app/LoginActivity.java
@@ -1,11 +1,15 @@
 package com.whuLoveStudyGroup.app;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -13,6 +17,7 @@ import android.view.View.OnClickListener;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -23,7 +28,9 @@ public class LoginActivity extends MyActivity {
     AutoCompleteTextView phone;
     EditText password;
     Button commit;
-
+    private static final String TAG = "dong.loginActivity";
+    ConnWithServer connWithServer = new ConnWithServer();
+    
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
 
@@ -47,7 +54,50 @@ public class LoginActivity extends MyActivity {
         commit.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
+                
+                if (TextUtils.isEmpty(phone.getText())){
+                    Toast.makeText(LoginActivity.this, "请输入手机号。", Toast.LENGTH_SHORT).show();
+                    return;
+                }else if (TextUtils.isEmpty(password.getText())){
+                    Toast.makeText(LoginActivity.this, "请输入密码。", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                
+                int error = connWithServer.loginUser(phone.getText().toString(),password.getText().toString());
 
+                switch (error) {
+                    case 0:
+                        finish();
+                        break;
+                    case 404021:
+                        Log.d(TAG, "sendRequest: " + error);
+                        Toast.makeText(LoginActivity.this, "请检查手机号是否有误。", Toast.LENGTH_SHORT).show();
+                        return;
+                    case 404031:
+                    case 404033:
+                    case 404034:
+                        Log.d(TAG, "sendRequest: " + error);
+                        Toast.makeText(LoginActivity.this, "密码不正确。", Toast.LENGTH_SHORT).show();
+                        return;
+                    case 404023:
+                        Log.d(TAG, "sendRequest: " + error);
+                        Toast.makeText(LoginActivity.this, "该手机号尚未注册。", Toast.LENGTH_SHORT).show();
+                        return;
+                    case 400:
+                    case 402:
+                    case 999:
+                        Log.d(TAG, "sendRequest: " + error);
+                        Toast.makeText(LoginActivity.this, "出现错误" + error + ",请联系我们，谢谢！", Toast.LENGTH_SHORT).show();
+                        return;
+                    case 499:
+                        Log.d(TAG, "sendRequest: " + error);
+                        Toast.makeText(LoginActivity.this, "服务器连接失败，请重试。", Toast.LENGTH_SHORT).show();
+                        return;
+                    default:
+                        Log.d(TAG, "sendRequest: " + error);
+                        Toast.makeText(LoginActivity.this, "出现技术性错误" + error + ",请联系我们，谢谢！", Toast.LENGTH_SHORT).show();
+                        return;
+                }
             }
         });
 
